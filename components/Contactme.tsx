8b8095a13d2978726793135d109e2bafc5bfbd99
jai <jaianand5789@gmail.com>
2024-03-05 02:18:29 +0530

    responsiveness

--- components/Contactme.tsx
+++ components/Contactme.tsx
@@ -61,24 +61,24 @@ export default function Contactme({ }: Props) {
         }
     };
   return (
-    <>
-        <div className="py-8 lg:pt-20 px-4 mx-auto max-w-screen-md mt-4">
-              <motion.h2 {...commonMotionProperties} className="mb-5 text-center uppercase tracking-widest text-xl text-[#aaaaaa]"><span className='shadow-sm shadow-[#878686]'>Contact ME</span></motion.h2>
-              <motion.p {...commonMotionProperties} className="mb-6 lg:mb-12 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl tracking-wider">Wanna share a feedback? Would Like to work with me? Wanna to reach out to me? </motion.p>
-        <form onSubmit={handleSubmit} className="space-y-4">
+    <div className='h-screen flex flex-col items-center justify-between pt-24'>
+        <div className="px-4 mx-auto max-w-screen-md">
+              <motion.h2 {...commonMotionProperties} className="mb-16 md:mb-4 text-center uppercase tracking-widest text-xl text-[#aaaaaa]"><span className='shadow-sm shadow-[#878686]'>Contact ME</span></motion.h2>
+              <motion.p {...commonMotionProperties} className="mb-6 font-light text-center text-gray-500 dark:text-gray-400 sm:text-md tracking-wider">Wanna share a feedback? Would Like to work with me? Wanna to reach out to me? </motion.p>
+        <form onSubmit={handleSubmit} className="space-y-2">
             <div>
-                <motion.label {...commonMotionProperties} htmlFor="email" className="block mb-2 text-md font-medium text-gray-900 dark:text-gray-300">Your email</motion.label>
+                <label {...commonMotionProperties} htmlFor="email" className="block mb-2 text-md font-base text-sm text-gray-900 dark:text-gray-300">Your email</label>
                 <input type="email" id="email" value={formData.email} onChange={handleChange} className="focus:outline-none shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-[#535353] dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light hover:scale-105 transition duration-300 hover:border-rose-50" placeholder="johndoe@gmail.com" required />
             </div>
             <div>
-                <motion.label {...commonMotionProperties} htmlFor="subject" className="block mb-2 text-md font-medium text-gray-900 dark:text-gray-300">Subject</motion.label>
+                <label {...commonMotionProperties} htmlFor="subject" className="block mb-2 text-md font-base text-sm text-gray-900 dark:text-gray-300">Subject</label>
                 <input type="text" id="subject" value={formData.subject} onChange={handleChange} className="focus:outline-none block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-[#535353] dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light hover:scale-105 transition duration-300 hover:border-rose-50" placeholder="Subject" required />
             </div>
             <div className="sm:col-span-2">
-                <motion.label {...commonMotionProperties} htmlFor="message" className="block mb-2 text-md font-medium text-gray-900 dark:text-gray-300">Your message</motion.label>
+                <label {...commonMotionProperties} htmlFor="message" className="block mb-2 text-md font-base text-sm text-gray-900 dark:text-gray-300">Your message</label>
                 <textarea id="message" rows={4} value={formData.message} onChange={handleChange} className="focus:outline-none block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-[#535353] dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 hover:scale-105 transition duration-300 hover:border-rose-50" placeholder="Leave a feedback..."></textarea>
             </div>
-                  <motion.button {...commonMotionProperties} type="submit" className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-[#151515] sm:w-fit hover:bg-black focus:ring-2 focus:ring-primary-300 dark:focus:ring-primary-800 transition duration-200">Send message</motion.button>
+                  <button {...commonMotionProperties} type="submit" className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-[#151515] sm:w-fit hover:bg-black focus:ring-2 focus:ring-primary-300 dark:focus:ring-primary-800 transition duration-200">Send message</button>
               </form>
               <ToastContainer
                 position="bottom-right"
@@ -103,5 +103,5 @@ export default function Contactme({ }: Props) {
             </li>
         </ul>
     </div>
-    </>)
+    </div>)
 }
--- components/Skills.tsx
+++ components/Skills.tsx
@@ -12,10 +12,10 @@ export default function Skills({}: Props) {
     <div className='flex flex-col h-screen gap-10 md:gap-6 2xl:gap-8 items-center'>
       <motion.div className='uppercase mt-24 tracking-widest text-xl text-[#aaaaaa] shadow-sm shadow-[#878686]' {...commonMotionProperties}>
         Skills</motion.div>
-        <motion.div className='mx-8 uppercase text-lg tracking-wider text-[#aaaaaa] flex flex-col items-center md:flex-row'><span>My TechStack,&nbsp;</span><strong>but I&apos;m open to Learn&nbsp;</strong> 🚀🚀🚀</motion.div>
+        <motion.div {...commonMotionProperties} className='mx-8 uppercase text-lg tracking-wider text-[#aaaaaa] flex flex-col items-center md:flex-row'><span>My TechStack,&nbsp;</span><strong>but I&apos;m open to Learn&nbsp;</strong> 🚀🚀🚀</motion.div>
       <div  {...commonMotionProperties} className="mx-4 max-w-md flex flex-row gap-4 flex-wrap justify-center items-center overflow-y-hidden overflow-x-hidden">
         {skillData.map((skill) => <Skills_1 key={skill.id} props={skill} />)}
-        <div className='mt-2 p-4 text-center md:hidden uppercase tracking-wide text-base text-[#aaaaaa] font-bold'>Continuous learner with a dynamic skill ready for impactful contributions.</div>
+        <motion.div {...commonMotionProperties} className='mt-2 p-4 text-center md:hidden uppercase tracking-wide text-base text-[#aaaaaa] font-bold'>Continuous learner with a dynamic skill ready for impactful contributions.</motion.div>
       </div>
     </div>
   )
